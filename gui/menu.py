cf2d48fb738a9169a463be293e64a11fb281b2e3
isac <grojisac@gmail.com>
2021-07-26 23:12:58 +0200

    tkinter

--- gui/menu.py
+++ gui/menu.py
@@ -1,6 +1,8 @@
 from tkinter import *
 
-
 main = Tk()
 main.title("Sistema de Gestao de vendas")
+main.geometry("1000x500")
+main.resizable(False, False)
+main.iconbitmap("icons/shopping_cart.ico")
 main.mainloop()
