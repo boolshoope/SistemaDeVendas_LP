123d05a039e1a9220a5e1474950ece5f26f18120
Isidro Bata <bata.isidro@gmail.com>
2021-07-31 23:18:49 +0200

    Conexão com bd
    Signed-off-by: Isidro Bata <bata.isidro@gmail.com>

--- config/config.py
+++ config/config.py
@@ -1,13 +1,14 @@
 # Constantes para conectar a bd
 
-HOST = str('127.0.0.1')
-DB = str('Look at me ')
-USER = str('not find')
-PASSWORD = str('empty')
-PORT = str('3306')
-
 global HOST
 global DB
 global USER
 global PASSWORD
 global PORT
+
+HOST = str('sql303.epizy.com')
+DB = str('epiz_25270403_sistema')
+USER = str('epiz_25270403')
+PASSWORD = '83OaASH78CflWUB'
+PORT = str('3306')
+
--- libs/database.py
+++ libs/database.py
@@ -5,10 +5,36 @@ from config import config
 
 class Database():
     def __init__(self):
-        print("")
+        print("DB")
 
     def connect(self):
         try:
-            data = mysql.connector.connect(config.HOST, config.USER, config.PASSWORD, config.PORT, config.DB)
+            data = mysql.connector.connect(
+                host=config.HOST,
+                user=config.USER,
+                password=config.PASSWORD,
+                port=config.PORT,
+                database=config.DB
+            )
+
+            query = data.cursor()
+            query.execute('SELECT * FROM produto')
+
+            categoria = query.fetchall()
+
+            for cat in categoria:
+                print(cat)
+                print('nome'+cat[1])
+                print('descricao'+cat[2])
+                print('preco'+cat[3])
+                print('stock'+cat[4])
+                print('codBarras'+cat[5])
+                print('idCategoria'+cat[6])
+
+
         except ModuleNotFoundError:
             print("algo deu errado")
+
+
+db = Database()
+db.connect()
