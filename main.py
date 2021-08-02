9c23cf2c8e5941614562b07f2b8b8d22377eba9b
Isidro Bata <bata.isidro@gmail.com>
2021-08-02 19:26:34 +0200

    Menu
    Signed-off-by: Isidro Bata <bata.isidro@gmail.com>

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-#from views.gui import login
 from libs import app
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-## Aplicação
-
-#addFuncionario.callAddFuncionario()
-#login.callLogin()
 app.App()
