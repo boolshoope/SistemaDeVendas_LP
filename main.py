cf9f53751e7f859c2a66e009ed73f3dcfc66a1c8
isac <grojisac@gmail.com>
2021-08-02 19:34:25 +0200

    Operacoes Login
    Signed-off-by: isac <grojisac@gmail.com>

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+#from views.gui import login
 from libs import app
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#
+##### Aplicação
+#
+
+#login.callLogin()
 app.App()
